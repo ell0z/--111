0274fc45f02ebd9c1b9f3be6567ae3f5ca895586
ellp820 <129738235+ell0z@users.noreply.github.com>
2023-04-16 22:07:01 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,27 +1,3 @@
-#include <iostream>
-
-using namespace std;
-
-void solve() {
-    int n, d;
-    cin >> n >> d;
-    string s;
-    cin >> s;
-    for (int i = 0; i < n; ++i) {
-        if (s[i] - '0' >= d) {
-            cout << s[i];
-        }
-        else {
-            cout << d;
-            for (int j = i; j < n; ++j) {
-                cout << s[j];
-            }
-            cout << '\n';
-            return;
-        }
-    }
-    cout << d << '\n';
-}
 
 int main() {
     int t;
