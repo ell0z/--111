4308a3fd14d00544ae08422a6f77b6c94524d095
ellp820 <129738235+ell0z@users.noreply.github.com>
2023-04-16 22:32:38 +0300

    Create Source.cpp

---
+++ Source.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, j, n, m, a[3][3];
+	cin >> n;
+	cin >> m;
+	for (i = 0; i, n; i++) {
+		for (j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+
+	}
+	for (i = 0; i, n; i++) {
+		for (j = 0; j < m; j++) {
+			cout << a[i][j]<<"\t";
+
+		}
+		cout << endl;
+	}
+	
+
+
+}
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i, j, n, m, a[3][3];
+	cin >> n;
+	cin >> m;
+	for (i = 0; i, n; i++) {
+		for (j = 0; j < m; j++) {
+			cin >> a[i][j];
+		}
+
+	}
+	for (i = 0; i, n; i++) {
+		for (j = 0; j < m; j++) {
+			cout << a[i][j] << "\t";
+
+		}
+		cout << endl;
+	}
+
+
+
+}
